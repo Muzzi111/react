f19781975407e1061d5e7dee7f7823fb571d97c2
Marigold <uchemarigold@gmail.com>
2025-06-26 13:31:03 +0100

    test cols

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className="flex items-center justify-center h-dvh">
-      <h1 className="text-red-500 text-3xl font-bold"> This is the main branch</h1>
+      <h1 className="text-gray-500 text-3xl font-bold"> This is the main branch</h1>
     </div>
 
   )
